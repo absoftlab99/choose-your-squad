dd842ed1ade6027f70b505de17b712e4814e178c
Abdullah Al Mahmud <58851539+absoftlab99@users.noreply.github.com>
2022-08-22 15:58:46 +0600

    new function create for get list length and multiplectation with player price

--- js/calculation.js
+++ js/calculation.js
@@ -1,15 +1,14 @@
 //player expence
 document.getElementById('calculate').addEventListener('click', function(){
     const playerPrice = fieldValue('player-price')
-    const totalPlayerPrice = playerPrice * 5;
+    const totalPlayerPrice = playerPrice * listLenght();
     const playerExpenceText = document.getElementById('player-expence');
     playerExpenceText.innerText = totalPlayerPrice;
     return totalPlayerPrice;
 })
 
-
 document.getElementById('calculate-total').addEventListener('click', function(){
-    const player = fieldValue('player-price');
+    const player = fieldValue('player-price') * listLenght();
     const manager = fieldValue('manager');
     const coach = fieldValue('coach');
     const total = player + manager + coach;
@@ -17,6 +16,11 @@ document.getElementById('calculate-total').addEventListener('click', function(){
     totalText.innerText = total;
 })
 
+function listLenght(){
+    const players = document.querySelectorAll('li');
+    const playersNumber = players.length;
+    return playersNumber;
+}
 //player + manager + cocha expence
 function fieldValue(id){
     const managerCoachField = document.getElementById(id);
@@ -24,5 +28,3 @@ function fieldValue(id){
     const managerCoach = parseFloat(managerCoachString);
     return managerCoach;
 }
-
-
