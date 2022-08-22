4163fbfec58935b0147b22b55e4cf9f3d5bcc3c1
Abdullah Al Mahmud <58851539+absoftlab99@users.noreply.github.com>
2022-08-22 14:18:00 +0600

    full calculation completed

--- js/calculation.js
+++ js/calculation.js
@@ -1,13 +1,28 @@
+//player expence
 document.getElementById('calculate').addEventListener('click', function(){
-    //get data from field
-    const playerPriceField = document.getElementById('player-price');
-    const playerPriceString = playerPriceField.value;
-    const playerPrice = parseFloat(playerPriceString);
+    const playerPrice = fieldValue('player-price')
     const totalPlayerPrice = playerPrice * 5;
-
     const playerExpenceText = document.getElementById('player-expence');
-
     playerExpenceText.innerText = totalPlayerPrice;
+    return totalPlayerPrice;
 })
+
+
+document.getElementById('calculate-total').addEventListener('click', function(){
+    const player = fieldValue('player-price');
+    const manager = fieldValue('manager');
+    const coach = fieldValue('coach');
+    const total = player + manager + coach;
+    const totalText = document.getElementById('total');
+    totalText.innerText = total;
+})
+
+//player + manager + cocha expence
+function fieldValue(id){
+    const managerCoachField = document.getElementById(id);
+    const managerCoachString = managerCoachField.value;
+    const managerCoach = parseFloat(managerCoachString);
+    return managerCoach;
+}
 
 
